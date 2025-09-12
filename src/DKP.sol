71d4294a583d0b2fc62ed3ea34cf8e82c6f721b6
timburman <aryankaushik251@gmail.com>
2025-09-12 09:54:20 +0530

    DKP: Minor standard changes, getter function

--- src/DKP.sol
+++ src/DKP.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract DKP is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract DKP is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable {
     struct Submission {
         uint256 id;
         bytes32 contentHash;
@@ -16,38 +16,40 @@ contract DKP is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
         uint256 downVotes;
     }
 
-    uint256 public idCounter;
+    uint256 private _idCounter;
 
     // -- Mappings --
-    mapping(uint256 => Submission) internal submissions;
+    mapping(uint256 => Submission) public submissions;
 
     // Mapping SubmissionId => (Voter => hasVoted)
-    mapping(uint256 => mapping(address => bool)) internal hasVoted;
+    mapping(uint256 => mapping(address => bool)) public hasVoted;
 
     // -- Events --
-    event ContentSubmitted(uint256 Id, address indexed author);
-    event Voted(uint256 Id, address indexed user);
+    event ContentSubmitted(uint256 indexed Id, address indexed author);
+    event Voted(uint256 indexed Id, address indexed user);
 
     // Initializer
     function initialize(address initialOwner) public initializer {
         __Ownable_init(initialOwner);
         __ReentrancyGuard_init();
+
+        _idCounter = 1;
     }
 
     // Public and External functions
 
     function submitContent(bytes32 _contentHash) public returns (uint256 id) {
-        idCounter++;
-        Submission storage s = submissions[idCounter];
+        uint newId = _idCounter++;
+        Submission storage s = submissions[newId];
 
-        s.id = idCounter;
+        s.id = newId;
         s.contentHash = _contentHash;
         s.author = msg.sender;
         s.timestamp = block.timestamp;
 
-        emit ContentSubmitted(idCounter, msg.sender);
+        emit ContentSubmitted(newId, msg.sender);
 
-        return idCounter;
+        return newId;
     }
 
     function vote(uint256 submissionId, bool _isUpvote) external nonReentrant {
@@ -72,7 +74,10 @@ contract DKP is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
         return submissions[submissionId];
     }
 
-    function getHasVoted(uint256 submissionId, address user) external view returns (bool) {
-        return hasVoted[submissionId][user];
+    function getCurrentId() external view returns (uint) {
+        return _idCounter;
     }
+
+    // -- Upgradable Functionality --
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
