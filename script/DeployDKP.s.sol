8daee8d07045e5b33a1ff0224f40237f0423c15f
timburman <aryankaushik251@gmail.com>
2025-09-12 11:33:31 +0530

    DKPScript: Deployment script created

---
+++ script/DeployDKP.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/DKP.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployDKP is Script {
+    function run() external returns (address) {
+        address initialOwner = msg.sender;
+
+        vm.startBroadcast();
+
+        DKP implementation = new DKP();
+        console.log("Step 1: DKP Implementation deployed to address:", address(implementation));
+
+        bytes memory data = abi.encodeWithSignature("initialize(address)", initialOwner);
+
+        ERC1967Proxy prxy = new ERC1967Proxy(address(implementation), data);
+        console.log("Step 2: DKP Proxy Deployed at address:", address(prxy));
+
+        vm.stopBroadcast();
+
+        return address(prxy);
+    }
+}
