ae62d6368eb9ffe6c59cbbd3ae3ba8237619743e
aryan <aryankaushik251@gmail.com>
2025-10-18 12:16:40 +0530

    DKPTest: Adjusted according to improved dkp contract, fix imports

--- test/DKP.t.sol
+++ test/DKP.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
-import "../src/DKP.sol";
+import {Test} from "forge-std/Test.sol";
+import {DKP} from "../src/DKP.sol";
+import {DKPToken} from "../src/DKPToken.sol";
 
 contract DKPTest is Test {
     address public owner = makeAddr("Owner");
@@ -10,14 +11,23 @@ contract DKPTest is Test {
     address public voter1 = makeAddr("voter1");
     address public voter2 = makeAddr("voter2");
     DKP public dkp;
+    DKPToken public dkpToken;
 
-    event ContentSubmitted(uint256 indexed Id, address indexed author);
-    event Voted(uint256 indexed Id, address indexed user);
+    event ContentSubmitted(uint256 indexed id, address indexed author);
+    event Voted(uint256 indexed id, address indexed user);
 
     function setUp() public {
         dkp = new DKP();
+        dkpToken = new DKPToken();
 
-        dkp.initialize(owner);
+        dkpToken.initialize(owner);
+        dkp.initialize(owner, address(dkpToken));
+
+        vm.prank(owner);
+        dkp.supplyReputation(user);
+
+        vm.prank(owner);
+        dkpToken.mint(address(dkp), 1000 ether);
     }
 
     function submissionOfContent() public returns (uint256 id) {
@@ -57,14 +67,14 @@ contract DKPTest is Test {
         uint256 id = submissionOfContent();
 
         vm.prank(voter1);
-        dkp.vote(id, true);
+        dkp.vote(id, 5, true);
 
         vm.prank(voter2);
-        dkp.vote(id, true);
+        dkp.vote(id, 5, true);
 
         DKP.Submission memory s = dkp.getSubmissions(id);
 
-        assertEq(s.upVotes, 2);
+        assertEq(s.upVotes, 10);
         assertEq(s.downVotes, 0);
     }
 
@@ -74,18 +84,18 @@ contract DKPTest is Test {
         vm.prank(voter1);
         vm.expectEmit(true, true, false, false);
         emit Voted(id, voter1);
-        dkp.vote(id, true);
+        dkp.vote(id, 5, true);
     }
 
     function test_FailVoteTwice() public {
         uint256 id = submissionOfContent();
 
         vm.prank(voter1);
-        dkp.vote(id, true);
+        dkp.vote(id, 5, true);
 
         vm.prank(voter1);
         vm.expectRevert("Already Voted");
-        dkp.vote(id, false);
+        dkp.vote(id, 5, false);
     }
 
     function test_FailVoteOnNonExistentSubmission(uint256 randomId) public {
@@ -93,7 +103,7 @@ contract DKPTest is Test {
         submissionOfContent();
 
         vm.prank(voter1);
-        vm.expectRevert("Invalid Submission Id");
-        dkp.vote(randomId, true);
+        vm.expectRevert("DKP: Invalid Submission Id");
+        dkp.vote(randomId, 5, true);
     }
 }
