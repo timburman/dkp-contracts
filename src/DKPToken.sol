43ea48b8cacd62349a3241a2adff1722fb3ba920
aryan <aryankaushik251@gmail.com>
2025-10-18 12:15:34 +0530

    DKPToken: Fix Imports

--- src/DKPToken.sol
+++ src/DKPToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract DKPToken is ERC20Upgradeable, OwnableUpgradeable {
+contract DKPToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
     function initialize(address initialOwner) public initializer {
         __ERC20_init("DKP Token", "DKP");
         __Ownable_init(initialOwner);
