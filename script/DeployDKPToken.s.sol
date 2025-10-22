c7c61b0af3055617f0f5076ecea553263d040f51
aryan <aryankaushik251@gmail.com>
2025-10-22 15:29:38 +0530

    DeployDKPToken: Completed

--- script/DeployDKPToken.s.sol
+++ script/DeployDKPToken.s.sol
@@ -5,4 +5,20 @@ import {Script, console} from "forge-std/Script.sol";
 import {DKPToken} from "../src/DKPToken.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract DeployDKPToken is Script {}
+contract DeployDKPToken is Script {
+    function run() external returns (address) {
+        address initialOwner = msg.sender;
+        vm.startBroadcast();
+
+        DKPToken implementation = new DKPToken();
+        console.log("Token implementation deployed at:", address(implementation));
+
+        bytes memory data = abi.encodeWithSignature("initialize(address)", initialOwner);
+
+        ERC1967Proxy proxy = new ERC1967Proxy(address(implementation), data);
+        console.log("Token Proxy Deployed at:", address(proxy));
+
+        vm.stopBroadcast();
+        return address(proxy);
+    }
+}
