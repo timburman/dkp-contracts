a63a5879c33cebcf20f690a0841c0c7af5c36b9b
aryan <aryankaushik251@gmail.com>
2025-10-26 18:41:31 +0530

    Upgrad DKP proxy script

---
+++ script/UpgradeDKP.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DKP} from "../src/DKP.sol";
+
+contract UpgradeDKP is Script {
+    address public constant DKP_PROXY_ADDRESS = 0x6789E70ada20C853E12b8bae36085e60FeaAE7Eb;
+
+    function run() external {
+
+        vm.startBroadcast();
+
+        DKP newImplementation = new DKP();
+        console.log("New DKP implementation deployed at:", address(newImplementation));
+
+        DKP proxy = DKP(DKP_PROXY_ADDRESS);
+
+        proxy.upgradeToAndCall(address(newImplementation), bytes(""));
+        console.log("DKP Proxy has been upgraded");
+
+
+        vm.stopBroadcast();
+
+    }
+}
